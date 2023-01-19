0a237ffeaf0415c1ecde7b10f9dd5eb30054cfe2
Aakash Jha <42407874+1tsak@users.noreply.github.com>
2023-01-19 18:25:18 +0530

    Profile Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ticketbooking/screens/login.dart';
 import 'package:ticketbooking/utils/app_styles.dart';
 import './screens/bottom_bar.dart';
 
@@ -16,11 +17,9 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        
         primaryColor: primary,
       ),
-      home: const BottomBar(),
+      home: const Login(),
     );
   }
 }
-
--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:ticketbooking/utils/App_layout.dart';
 
 import '../utils/app_styles.dart';
 
@@ -8,12 +10,35 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Styles.bgcolor,
-      body: ListView(
-        children: [
-          
-        ],
-      ),
-    );
+        backgroundColor: Styles.bgcolor,
+        body: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: const [
+              Gap(125),
+              CircleAvatar(
+                  backgroundImage: AssetImage('assets/images/p1.png'),
+                  radius: 60),
+              Gap(10),
+              Text("Akshay", style: TextStyle(fontSize: 24)),
+              Gap(5),
+              Text(
+                "someone@example.com",
+                style: TextStyle(fontSize: 16, color: Colors.grey),
+              ),
+              Gap(10),
+              TextButton(
+                onPressed: null,
+                // ignore: sort_child_properties_last
+                child:  Padding(
+                  padding:  EdgeInsets.only(left:8.0,right:8.0),
+                  child: Text("Logout",style: TextStyle(color: Colors.white),),
+                ),
+                style: ButtonStyle(
+                    backgroundColor: MaterialStatePropertyAll(Colors.red)),
+              )
+            ],
+          ),
+        ));
   }
 }
